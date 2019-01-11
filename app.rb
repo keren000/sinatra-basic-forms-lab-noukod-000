4b374f4a47f1b9544c850fac9be43f836af646aa
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-11 16:21:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,5 +5,12 @@ class App < Sinatra::Base
     erb :index
   end
 
+  get '/new' do
+    erb :create_puppy
+  end
 
+  post '/' do
+    @new_pooch = Puppy.new(params[:name], params[:breed], params[:age])
+    erb :display_puppy
+  end
 end
